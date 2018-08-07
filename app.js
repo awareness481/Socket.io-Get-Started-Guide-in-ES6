2a36216a9c69d5f8933c103481abbb8119ff2ac4
Jesse Jafa <jesse.jafa@gmail.com>
2018-08-07 13:22:16 +0300

    app.js & index.html

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/routes/index.html');
+})
+
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+io.on('connection', (socket) => {
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg);
+  });
+});
+
+io.on('connection', (socket) => {
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(3000 , () => {
+  console.log('Server Initialized!');
+})
